5516f924fe03ea0355342d016839b3c54a47ed70
Pierre <pierrelidem@gmail.com>
2022-06-22 20:08:06 +0200

    correction

--- event/manipulation-du-DOM.js
+++ event/manipulation-du-DOM.js
@@ -1,11 +1,11 @@
-//premiere facon de selectionné
+//Première façon de sélectionner
 //Sélection sur notre document le id = "btn" dans le HTML
 const btn = document.querySelector('#btn');
 const btn2 = document.querySelector('#btn2');
 //deuxieme facon de selectionné
 const img = document.getElementById('img');
 
-//au boutton on ajoute un evenement  au click qui fais apparaitre limage
+//Au bouton, on ajoute un événement au click qui fait apparaître l'image.
 btn.addEventListener('click', () => {
 
     img.
