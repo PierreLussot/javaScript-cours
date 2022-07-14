652f38ab41c994672eb51af48b320618a7983c35
Pierre <pierrelidem@gmail.com>
2022-07-14 15:52:37 +0200

    ajout variable

---
+++ variable/variables.js
@@ -0,0 +1,24 @@
+/************LET*************/
+//let déclare une variable.
+let cours = 'hello world !';
+console.log(cours + '// avant mise a jour ');
+
+//Attribue à cours une autre valeur.
+ cours = 'goodbye world !';
+console.log(cours + '// apres mise a jour ');
+
+//let à des variables qui sont évolutives
+
+/************CONST*************/
+//const crée, une variable impossible a changé donc qui est immuable.
+const prenom = 'Patrice';
+console.log(prenom)
+
+prenom = 24
+console.log(prenom)
+
+
+
+
+
+
