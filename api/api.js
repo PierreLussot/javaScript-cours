71868651cb2747c7d3c4612e3d531673783d9102
Pierre <pierrelidem@gmail.com>
2022-07-14 15:53:19 +0200

    ajout api

--- api/api.js
+++ api/api.js
@@ -10,13 +10,19 @@ btn.addEventListener("click", (e) => {
 });
 
 const getCitation = () => {
-  fetch("https://api.quotable.io/random")
+  fetch("https://api.breakingbadquotes.xyz/v1/quotes")
     .then((reponse) => reponse.json())
     .then((data) => {
-       p1.innerHTML = "autheur : " + data.author;
+      console.log(data);
+
+       p1.innerHTML = "autheur : " + data[0].author;
+      p2.innerHTML = "citation : " + data[0].quote;
+      
+
+      /*  p1.innerHTML = "autheur : " + data.author;
       p2.innerHTML = "citation : " + data.content;
-      p3.innerHTML = "aparrition : " + data.dateAdded;
- 
+      p3.innerHTML = "aparrition : " + data.dateAdded; */
+
       /*  p1.innerHTML = "autheur : " + data.personnage;
     p2.innerHTML = "citation : " + data.citation;
     p3.innerHTML = "aparrition : " + data.saison; */
@@ -31,3 +37,5 @@ getCitation();
 
 //Api citation kaamelott      https://kaamelott.chaudie.re/api/random
 //Api citation quotable   https://api.quotable.io/random
+//Api citation anime  https://animechan.vercel.app/api/random
+//Api citation breaking-bad     https://breaking-bad-quotes.herokuapp.com/v1/quotes
